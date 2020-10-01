ced1a86b02d50886527058b6c5dbf6fb5a3f436b
KurokuTetsuya <perumnas.good@gmail.com>
2020-10-01 17:35:42 +0700

    feat(initial): Page very not responsive rn, soon

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<div class="jumbotron d-flex align-items-center min-vh-100 text-white">
+	<div class="container">
+		<div style="display: flex; justify-content: space-between;">
+			<div class="flex-column">
+				<h1>FrutBits Indonesia</h1>
+				<p>Sebuah komunitas Discord dari Indonesia bertemakan	
+				general, gaming dan lain lain!
+				</p>
+			</div>
+			<div>
+				<iframe title="Discord Widget" src="https://discordapp.com/widget?id=715165490246582313&amp;theme=dark" width="350" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+			</div>
+		</div>
+	</div>
+</div>
